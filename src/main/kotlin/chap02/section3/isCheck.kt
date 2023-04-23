66ea4b9f0471235b589cad999678141af077cf3a
hong <sdhong0609@gmail.com>
2023-04-23 16:14:20 +0900

    Chapter 02 Section3 공부 완료

---
+++ src/main/kotlin/chap02/section3/isCheck.kt
@@ -0,0 +1,11 @@
+package chap02.section3
+
+fun main() {
+    val num = 256
+
+    if (num is Int) { // 1.num이 Int형일 때
+        print(num)
+    } else if (num !is Int) { // 2.num이 Int형이 아닐 때, !(num is Int)와 동일
+        print("Not a Int")
+    }
+}
