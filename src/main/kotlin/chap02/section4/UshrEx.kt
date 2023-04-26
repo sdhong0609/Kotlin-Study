2ca98f107d8989a98391d429ea46f87781dbbc43
hong <sdhong0609@gmail.com>
2023-04-27 00:51:37 +0900

    Chapter 02 Section4 공부 완료

---
+++ src/main/kotlin/chap02/section4/UshrEx.kt
@@ -0,0 +1,11 @@
+package chap02.section4
+
+fun main() {
+    val number1 = 5
+    val number2 = -5
+
+    println(number1 shr 1)
+    println(number1 ushr 1) // 변화 없음 (number1 shr 1과 같은 결과)
+    println(number2 shr 1) // 부호 비트가 1로 유지
+    println(number2 ushr 1) // 부호 비트가 0이 되면서 변경
+}
